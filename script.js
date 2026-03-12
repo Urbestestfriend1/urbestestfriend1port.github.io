299d2933b050edd5f7fcf20e9014f113e9e48c29
Urbestestfriend1 <littleblockbldr@gmail.com>
2026-03-12 16:38:02 -0400

    Refactor scrollToSection function and add IntersectionObserver

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function scrollToSection(id)
-{
+function scrollToSection(id){
 document.getElementById(id).scrollIntoView({
 behavior:"smooth"
 });
 }
+
+
+
+const faders = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("visible");
+
+}
+
+});
+
+});
+
+
+faders.forEach(section => {
+
+observer.observe(section);
+
+});
